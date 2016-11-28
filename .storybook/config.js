32f83c1d40edb0bcfeddc747098e45695d75c4da
Thomas Roth <thomas.roth@students.fhnw.ch>
2016-11-28 15:36:31 +0100

    add storybook

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import { configure /*, addDecorator, setAddon */ } from '@kadira/storybook';
+/* import centered from '@kadira/react-storybook-decorator-centered';
+import infoAddon from '@kadira/react-storybook-addon-info';
+
+
+import '../src/style/app.css';
+
+setAddon(infoAddon);
+
+addDecorator(centered);
+ */
+
+const req = require.context('../', true, /\.story\.jsx$/);
+
+function loadStories() {
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
