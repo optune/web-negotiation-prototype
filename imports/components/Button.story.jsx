06fc3618a091a30448a5ce7e26eac401784b289a
Thomas Roth <thomas.roth@students.fhnw.ch>
2016-11-29 12:22:01 +0100

    add icons2

--- imports/components/Button.story.jsx
+++ imports/components/Button.story.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { storiesOf} from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 
 import Button from './Button.jsx';
 
-storiesOf("element/Button", module)
+storiesOf('element/Button', module)
 .add('Button', () =>
-	(
-		<Button title="Bla" />
-	)
+(
+  <Button title="Bla" />
+),
 )
 .add('Button 2', () =>
-	(
-		<Button title="Blabla" />
-	),
+(
+  <Button title="Blabla" />
+),
 );
--- imports/components/Switch.jsx
+++ imports/components/Switch.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Switch = props => (
   <div className="onoffswitch">
-    <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" defaultChecked={props.checked}/>
+    <input type="checkbox" name="onoffswitch" className="onoffswitch-checkbox" id="myonoffswitch" defaultChecked={props.checked} />
     <label className="onoffswitch-label" htmlFor="myonoffswitch" />
   </div>
 );
