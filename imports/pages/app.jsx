ad03291185a75dd1c236784147bf8c5316262a61
Simon Schmid <simon@optune.me>
2016-11-30 18:03:53 +0100

    basic auth0 login/logout

--- imports/pages/app.jsx
+++ imports/pages/app.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Meteor } from 'meteor/meteor';
 
 
-export default () => (
-  <h1>Hello World</h1>
+const App = props => (
+  <div className="flex-center-middle filling">
+    {props.showLogin ?
+      <button onClick={props.login}>Login</button>
+    :
+      <div>
+        <h1>Hello {props.user ? props.user.services.auth0.email : undefined}</h1>
+        <button onClick={props.logout}>Logout</button>
+      </div>
+    }
+  </div>
 );
 
+App.propTypes = {
+  showLogin: React.PropTypes.bool,
+  login: React.PropTypes.func,
+  logout: React.PropTypes.func,
+  user: React.PropTypes.object,
+};
+
+const AppContainer = createContainer((props) => {
+  console.log(Meteor.user(), props);
+
+  const newProps = {
+    ...props,
+    user: Meteor.user(),
+    showLogin: !Meteor.user() && !Meteor.loggingIn(),
+    login: () => Meteor.loginWithAuth0(),
+    logout: () => Meteor.logout(),
+  };
+
+  return newProps;
+}, App);
+
+
+export default AppContainer;
