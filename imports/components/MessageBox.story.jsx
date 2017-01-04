5cfa8fd78e3c2382db0f6311b992e729afb49a78
Thomas Roth <thomas.roth@students.fhnw.ch>
2017-01-04 13:53:42 +0100

    MessageBox

--- imports/components/MessageBox.story.jsx
+++ imports/components/MessageBox.story.jsx
@@ -26,31 +26,82 @@ storiesOf('organisms/MessageBox', module)
           body: 'Hi there, Id like to book you to our awesome party. Are you interested?',
           date: '23.12.2016',
           time: '22:05',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
           body: 'Oh, hi',
           date: '23.12.2016',
           time: '22:06',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
           body: 'Yes, of course! But I need mo money!',
           date: '23.12.2016',
           time: '22:07',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: true,
           body: 'Alright, we`ll give you as much as you want. Cool?',
           date: '23.12.2016',
           time: '22:30',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
           body: 'Good boy',
           date: '23.12.2016',
           time: '22:35',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
+        },
+        {
+          self: false,
+          body: 'Fee is to low',
+          date: '23.12.2016',
+          time: '22:35',
+          type: 'quick',
+          userPicture: 'https://placeimg.com/100/100/people',
+          changes: [{
+            object: 'Fee is to low',
+          }, {
+            object: 'Date not avialable',
+          }],
+        }, {
+          self: false,
+          body: 'Hallo',
+          date: '23.12.2016',
+          time: '22:35',
+          userPicture: 'https://placeimg.com/100/100/people',
+          type: 'system',
+          changes: [{
+            object: 'Fee',
+            from: '800',
+            to: '1000',
+          }, {
+            object: 'Deal',
+            from: 'Landed',
+            to: 'Flat',
+          }, {
+            object: 'Performance',
+            from: 'DJ',
+            to: 'Live',
+          }, {
+            object: 'Duration',
+            from: '1.5',
+            to: '2',
+          }, {
+            object: 'Date',
+            from: '24.12.2016',
+            to: '31.12.2016',
+          }],
         }]}
       />
     </div>
--- imports/components/Negotiation.story.jsx
+++ imports/components/Negotiation.story.jsx
@@ -17,38 +17,89 @@ storiesOf('Negotiation', module)
       <MessageBox
         messages={[{
           self: true,
-          body: 'Hi there, Id like to book you to our awesome party. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Are you interested?',
+          body: 'Hi there, Id like to book you to our awesome party. Are you interested?',
           date: '23.12.2016',
           time: '22:05',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
           body: 'Oh, hi',
           date: '23.12.2016',
           time: '22:06',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
-          body: 'Yes, of course! But I need mo money! Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+          body: 'Yes, of course! But I need mo money!',
           date: '23.12.2016',
           time: '22:07',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: true,
           body: 'Alright, we`ll give you as much as you want. Cool?',
           date: '23.12.2016',
           time: '22:30',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
         }, {
           self: false,
           body: 'Good boy',
           date: '23.12.2016',
           time: '22:35',
+          type: 'user',
           userPicture: 'https://placeimg.com/100/100/people',
+          changes: [],
+        },
+        {
+          self: false,
+          body: 'Fee is to low',
+          date: '23.12.2016',
+          time: '22:35',
+          type: 'quick',
+          userPicture: 'https://placeimg.com/100/100/people',
+          changes: [{
+            object: 'Fee is to low',
+          }, {
+            object: 'Date not avialable',
+          }],
+        }, {
+          self: false,
+          body: 'Hallo',
+          date: '23.12.2016',
+          time: '22:35',
+          userPicture: 'https://placeimg.com/100/100/people',
+          type: 'system',
+          changes: [{
+            object: 'Fee',
+            from: '800',
+            to: '1000',
+          }, {
+            object: 'Deal',
+            from: 'Landed',
+            to: 'Flat',
+          }, {
+            object: 'Performance',
+            from: 'DJ',
+            to: 'Live',
+          }, {
+            object: 'Duration',
+            from: '1.5',
+            to: '2',
+          }, {
+            object: 'Date',
+            from: '24.12.2016',
+            to: '31.12.2016',
+          }],
         }]}
       />
 
-      <MessageInput label="MESSAGE" messagehint="Your message" />
+      <MessageInput label="MESSAGE" />
     </div>
   </div>
 ))
